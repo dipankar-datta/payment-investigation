2f920f2f5aec4bb3a92f2f6b5e9f0fe87d546264
Dipankar Datta <dattapro5@gmail.com>
2022-03-20 00:16:57 +0530

    Enabled caching for ConfigurationsService.

--- src/main/java/com/application/paymentinvestigation/PaymentInvestigation.java
+++ src/main/java/com/application/paymentinvestigation/PaymentInvestigation.java
@@ -2,8 +2,10 @@ package com.application.paymentinvestigation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PaymentInvestigation {
 
 	public static void main(String[] args) {
--- src/main/java/com/application/paymentinvestigation/services/ConfigurationsService.java
+++ src/main/java/com/application/paymentinvestigation/services/ConfigurationsService.java
@@ -2,6 +2,7 @@ package com.application.paymentinvestigation.services;
 
 import com.application.paymentinvestigation.data.repositories.DropdownDataRepository;
 import com.application.paymentinvestigation.rest.response.DropdownResponse;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,6 +18,7 @@ public class ConfigurationsService {
         this.dropdownDataRepository = dropdownDataRepository;
     }
 
+    @Cacheable("configurations")
     public Map<String, List<DropdownResponse>> getStaticConfigurations() {
         return this.dropdownDataRepository.findByIsActiveIs("y").stream().map(DropdownResponse::fromEntityToResponse).collect(
                 Collectors.groupingBy(DropdownResponse::getFieldName));
